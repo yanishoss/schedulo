e6c02d264c8660affd11476c7735eff882e6e005
Yanis HEDJEM <yanishproduction@gmail.com>
2020-07-02 15:16:03 +0200

    build: make github action effectively fail on test fail

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose build
-docker-compose up --abort-on-container-exit --force-recreate --exit-code-from go_test
-docker-compose down --remove-orphans
+docker-compose up --abort-on-container-exit --force-recreate --exit-code-from go_test && docker-compose down --remove-orphans
