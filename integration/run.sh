947bb2b5b027bf823fd64df0a851e60859d552cc
Yanis HEDJEM <yanishproduction@gmail.com>
2020-07-03 10:02:11 +0200

    test(integration): healthcheck

--- integration/run.sh
+++ integration/run.sh
@@ -2,13 +2,4 @@
 
 docker-compose down --remove-orphans || true
 docker-compose build
-docker-compose up --detach --force-recreate
-
-cd ../
-until nc localhost 9876
-do
-  echo ...
-  sleep 1
-done
-
-GOMOD111=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 SCHEDULO_ADDR=localhost:9876 go test --cover -v github.com/yanishoss/schedulo/cmd/schedulo_server && cd integration/ && docker-compose down --remove-orphans
+docker-compose up --force-recreate --abort-on-container-exit --exit-code-from go_test && docker-compose down --remove-orphans
