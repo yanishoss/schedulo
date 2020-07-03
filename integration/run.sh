a84cd224d55dbdd3535e6005a7be0bb4497f4bf2
Yanis HEDJEM <yanishproduction@gmail.com>
2020-07-03 09:36:27 +0200

    test(integration): wait for schedulo to be available

--- integration/run.sh
+++ integration/run.sh
@@ -5,4 +5,10 @@ docker-compose build
 docker-compose up --detach --force-recreate
 
 cd ../
+until nc localhost 9876
+do
+  echo ...
+  sleep 1
+done
+
 GOMOD111=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 SCHEDULO_ADDR=localhost:9876 go test --cover -v github.com/yanishoss/schedulo/cmd/schedulo_server && cd integration/ && docker-compose down --remove-orphans
