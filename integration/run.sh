913ea0c7b2cafafbf5ffd565147c0b8db5367052
Yanis HEDJEM <yanishproduction@gmail.com>
2020-07-03 09:06:39 +0200

    test(integration): change test suit to run on host machine instead of docker container

--- integration/run.sh
+++ integration/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+docker-compose down --remove-orphans || true
 docker-compose build
-docker-compose up --abort-on-container-exit --force-recreate --exit-code-from go_test && docker-compose down --remove-orphans
+docker-compose up --detach --force-recreate
+
+cd ../
+GOMOD111=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go test --cover -v github.com/yanishoss/schedulo/cmd/schedulo_server && cd integration/ && docker-compose down --remove-orphans
